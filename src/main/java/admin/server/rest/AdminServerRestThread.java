ae7daf1fc2a436100477228fbb5b8bda0045fab3
Francesco Torgano <francesco.torgano@outlook.it>
2023-05-03 19:25:55 +0200

    Increased loggin level of com.sun.jersey logging

--- src/main/java/admin/server/rest/AdminServerRestThread.java
+++ src/main/java/admin/server/rest/AdminServerRestThread.java
@@ -3,6 +3,8 @@ package admin.server.rest;
 import com.sun.jersey.api.container.httpserver.HttpServerFactory;
 import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class AdminServerRestThread {
 
@@ -11,6 +13,9 @@ public class AdminServerRestThread {
 
 
   public static void main(String[] args) throws IOException {
+    // removes unnecessary messages from jersey logger
+    Logger.getLogger("com.sun.jersey").setLevel(Level.SEVERE);
+
     HttpServer server = HttpServerFactory.create("http://"+HOST+":"+PORT+"/");
     server.start();
 
