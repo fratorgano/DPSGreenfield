9996c4df17f4361b132ec37d44cb82b369ed284c
franc <francesco.torgano@outlook.it>
2023-05-19 10:40:55 +0200

    Remove unused code from BufferImpl

--- src/main/java/cleaning_robot/pollution/BufferImpl.java
+++ src/main/java/cleaning_robot/pollution/BufferImpl.java
@@ -8,22 +8,11 @@ import java.util.List;
 
 public class BufferImpl implements Buffer {
     ArrayList<Measurement> buffer = new ArrayList<>(8);
-    // public ArrayList<Double> averages = new ArrayList<>();
 
     @Override
     public synchronized void addMeasurement(Measurement m) {
         buffer.add(m);
         if (buffer.size()==8) {
-            /*double sum=0;
-            for (int i = 0; i < 4; i++) {
-                double removed = buffer.remove(0).getValue();
-                sum += removed;
-            }
-            for (int i = 0; i < 4; i++) {
-                double peeked = buffer.get(i).getValue();
-                sum += peeked;
-            }
-            averages.add( ( sum / 8.0));*/
             this.notify();
         }
     }
@@ -34,11 +23,6 @@ public class BufferImpl implements Buffer {
         buffer.subList(0, 4).clear();
         return copy;
     }
-//    public synchronized List<Double> readAllAveragesAndClean() {
-//        ArrayList<Double> copy = new ArrayList<>(averages);
-//        averages.clear();
-//        return copy;
-//    }
 
     @Override
     public String toString() {
