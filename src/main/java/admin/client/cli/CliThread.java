af2770258886eb177e8ee6fc5a9addfbeff48c07
franc <francesco.torgano@outlook.it>
2023-05-19 11:26:27 +0200

    Add option to use number to choose operation and improve date input

--- src/main/java/admin/client/cli/CliThread.java
+++ src/main/java/admin/client/cli/CliThread.java
@@ -1,13 +1,13 @@
 package admin.client.cli;
 
 import admin.client.AdminClient;
-import cleaning_robot.CleaningRobot;
 import common.logger.MyLogger;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.time.Instant;
+import java.time.format.DateTimeParseException;
 
 public class CliThread extends Thread{
     private final AdminClient ac;
@@ -21,15 +21,19 @@ public class CliThread extends Thread{
     @Override
     public void run() {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         while(running) {
+            l.log("Available commands: robotList(0), averageN(1), averageTime(2), quit(9)");
             try {
                 String s = br.readLine();
                 l.log(s);
                 switch (s) {
+                    case "0":
                     case "robotList":
                         l.log("Robot list:");
                         l.log(ac.restGetRobots());
                         break;
+                    case "1":
                     case "averageN":
                         l.log("insert robot ID:");
                         String crpID = br.readLine();
@@ -37,13 +41,16 @@ public class CliThread extends Thread{
                         Integer n = Integer.valueOf(br.readLine());
                         l.log(ac.restGetAverageN(crpID,n));
                         break;
+                    case "2":
                     case "averageTime":
-                        l.log("insert starting date: (format like: '2011-12-03 10:15:30')");
-                        String start = br.readLine().trim().replace(' ','T')+"Z";
-                        Instant in = Instant.parse(start);
-                        l.log("insert ending date: (format like: '2011-12-03 10:15:30)");
-                        String end = br.readLine().trim().replace(' ','T')+"Z";
+                        String start = getInputDate(br,"starting");
+                        String end = getInputDate(br,"ending");
                         l.log(ac.restGetAverageN(start,end));
+                        break;
+                    case "9":
+                    case "quit":
+                        l.log("Quitting...");
+                        this.running = false;
                         break;
                     default:
                         l.error("Unrecognized command");
@@ -53,5 +60,19 @@ public class CliThread extends Thread{
                 l.error("Failed to read input from CLI: "+e.getMessage());
             }
         }
+    }
+    private String getInputDate(BufferedReader br, String type) throws IOException {
+        String s = null;
+        while(s==null) {
+            l.log("insert "+type+" date: (format like: '2011-12-03 10:15:30')");
+            s = br.readLine().trim().replace(' ','T')+"Z";
+            try {
+                Instant.parse(s);
+            }catch (DateTimeParseException e) {
+                l.error("Wrong input format");
+                s = null;
+            }
+        }
+        return s;
     }
 }
